c8b14371b983d825dbc7e516477ac4e3e58697d5
pmof13323 <mofradparsa@gmail.com>
2024-04-03 15:23:46 +1030

    new algorithm

--- DataCleaner.py
+++ DataCleaner.py
@@ -12,32 +12,41 @@ dfAlphabetical = df.sort_values(by= 'context')
 dfAlphabetical = dfAlphabetical.drop_duplicates()
 dfAlphabetical.reset_index(drop=True, inplace=True)
 
-# handling overlaps
-indicesDrop = []
-for index, row in dfAlphabetical.iterrows():
-    if index > 0:
-        prevRow = dfAlphabetical.iloc[index - 1]
+def overlapFinder(start, end, df):
+    cleanups = []
+    for i in range(start, end):
+        for j in range(i+1, end):
+            phrase1 = df.iloc[i]['phrase']
+            phrase2 = df.iloc[j]['phrase']
 
-        if row['context'] == prevRow['context']:
-
-            overlap = not (row['start_index'] > prevRow['end_index'] or row['end_index'] < prevRow['start_index'])
-            subOverlap = (row['phrase'] in prevRow['phrase']) or (prevRow['phrase'] in row['phrase'])
-            
-            if overlap and subOverlap:
-                
-                # retain the shorter phrase in case of overlap
-                if len(row['phrase']) < len(prevRow['phrase']):
-                    indicesDrop.append(row.name)
+            if phrase1 in phrase2 or phrase2 in phrase1:
+                # Note: Use append(i) or append(j), not append[i] or append[j]
+                if len(phrase1) > len(phrase2):
+                    cleanups.append(j)  # If phrase1 is longer, mark phrase2 for removal
                 else:
-                    indicesDrop.append(prevRow.name)
+                    cleanups.append(i)  # If phrase2 is longer or they are equal, mark phrase1 for removal
+    return cleanups
 
-# dropping identified overlapping indices
-uniqDrop = list(set(indicesDrop))
-dfAlphabetical = dfAlphabetical.drop(uniqDrop).reset_index(drop=True)
-dfAlphabetical = dfAlphabetical.drop(0).reset_index(drop=True)
+def rowDeleter(indicesDrop, df):
+    uniqDrop = list(set(indicesDrop))  # Ensure we have unique indices to drop
+    df.drop(uniqDrop, inplace=True)  # Drop rows based on the indices
+    df.reset_index(drop=True, inplace=True)  # Reset index after drop
+    return df
+
+# Identify groups by context and their start-end indices in the DataFrame
+context_groups = dfAlphabetical.groupby('context').apply(lambda x: (x.index[0], x.index[-1] + 1)).tolist()
+
+indicesDrop = []
+for start, end in context_groups:
+    # Find overlaps within each context group
+    overlaps = overlapFinder(start, end, dfAlphabetical)
+    indicesDrop.extend(overlaps)
+
+# Delete rows with overlaps
+dfCleaned = rowDeleter(indicesDrop, dfAlphabetical)
 
 # grouping by context
-grouped = dfAlphabetical.groupby('context').agg({
+grouped = dfCleaned.groupby('context').agg({
     'type': lambda x: list(x),
     'start_index': lambda x: list(x),
     'end_index': lambda x: list(x),
